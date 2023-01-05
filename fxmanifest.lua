e94f64b0b6244419b77981af65d0043aaf71bac1
Project Roleplay <84641832+FiveMLuxe@users.noreply.github.com>
2023-01-05 16:56:12 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Elusive'
+description 'Steal from cars'
+version 'legacy'
+
+client_scripts{
+    'client.lua'
+}
+
+server_scripts{
+    'server.lua'
+}
