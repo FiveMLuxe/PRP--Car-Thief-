cdc409effd07148944930aa688df430e4a8cfd21
Project Roleplay <84641832+FiveMLuxe@users.noreply.github.com>
2023-01-05 17:11:46 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,7 +2,7 @@ ESX = nil
 
 Citizen.CreateThread(function()
   while ESX == nil do
-    TriggerEvent('esx:esx:getSharedObject', function(obj) ESX = obj end)
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
     Citizen.Wait(30)
   end
 end)
